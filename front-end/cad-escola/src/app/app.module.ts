dcbe9511f622d96b8427b3557e1b237c6b6d92b2
Nailton Simões <nailtonsimoes@gmail.com>
2023-06-14 03:10:52 -0300

    criação de components no front

---
+++ front-end/cad-escola/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AlunoComponent } from './components/aluno/aluno.component';
+import { TurmaComponent } from './components/turma/turma.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AlunoComponent,
+    TurmaComponent,
+    HeaderComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
